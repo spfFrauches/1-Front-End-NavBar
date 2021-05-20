95b17ba7a7fbc7523ceb8ef724c58b83bb58aa6f
Saulo Frauches <saulo_pf@hotmail.com>
2021-05-20 15:59:05 -0300

    Subindo o projeto inicial

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    
+    <head>
+       <meta charset="utf-8"/>
+       <title>Front End - NavBar Exemple</title>
+       <link rel="stylesheet" href="style.css"/>
+    </head>
+    
+    <body>
+       
+        <header id="header">            
+            
+            <a id="logo" href="#">Logo</a>        
+            
+            <nav id="nav">  
+                
+                <button id="btn-mobile">Menu</button>
+                
+                <ul id="menu">
+                    
+                    <li><a href="/">Sobre</a></li>
+                    <li><a href="/">Produtos</a></li>
+                    <li><a href="/">Portifólio</a></li>
+                    <li><a href="/">Contato</a></li>
+                    
+                </ul>                
+            </nav>           
+        </header>
+        
+        <script src="style.js"></script>
+               
+    </body>
+    
+    
+</html>
